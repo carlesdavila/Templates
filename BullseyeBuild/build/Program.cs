d184640b539e012b13be88d328d10fe3dc120942
Carlos Davila <carlos.davila@asg.com>
2022-04-30 00:57:50 +0200

    build.csproj initial create

---
+++ BullseyeBuild/build/Program.cs
@@ -0,0 +1,8 @@
+﻿using static Bullseye.Targets;
+
+Target("make-tea", () => Console.WriteLine("Tea made."));
+Target("drink-tea", DependsOn("make-tea"), () => Console.WriteLine("Ahh... lovely!"));
+Target("walk-dog", () => Console.WriteLine("Walkies!"));
+Target("default", DependsOn("drink-tea", "walk-dog"));
+
+await RunTargetsAndExitAsync(args);
