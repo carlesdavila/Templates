b9dd60a54eb3879485870b48a7f7eeb5ecbb2be5
Carlos Davila <carlos.davila@asg.com>
2022-05-02 21:05:23 +0200

    added bullseye as build pipeline

---
+++ build/Program.cs
@@ -0,0 +1,24 @@
+﻿using static Bullseye.Targets;
+using static SimpleExec.Command;
+
+const string artifactsDir = "artifacts";
+
+Target(Targets.CleanArtifactsOutput, () =>
+{
+    if (Directory.Exists(artifactsDir)) Directory.Delete(artifactsDir, true);
+});
+
+Target(Targets.GenerateArtifacts, DependsOn(Targets.CleanArtifactsOutput), async () =>
+{
+    await RunAsync("dotnet", $"pack -o {Directory.CreateDirectory(artifactsDir).FullName} --nologo");
+});
+
+Target("default", DependsOn(Targets.GenerateArtifacts));
+
+await RunTargetsAndExitAsync(args);
+
+internal static class Targets
+{
+    public const string CleanArtifactsOutput = "clean-artifacts-output";
+    public const string GenerateArtifacts = "generate-artifacts";
+}
