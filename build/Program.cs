959cf7257c97f4258a1e66a3601245c4e9c998a8
Carlos Davila <carlos.davila@asg.com>
2022-05-04 22:04:58 +0200

    added test target in build

--- build/Program.cs
+++ build/Program.cs
@@ -8,12 +8,18 @@ Target(Targets.CleanArtifactsOutput, () =>
     if (Directory.Exists(artifactsDir)) Directory.Delete(artifactsDir, true);
 });
 
-Target(Targets.GenerateArtifacts, DependsOn(Targets.CleanArtifactsOutput), async () =>
+Target(Targets.GenerateArtifacts, DependsOn(Targets.CleanArtifactsOutput), async () => { await RunAsync("dotnet", $"pack -o {Directory.CreateDirectory(artifactsDir).FullName} --nologo"); });
+
+Target(Targets.RunTests, DependsOn(Targets.GenerateArtifacts), async () =>
 {
-    await RunAsync("dotnet", $"pack -o {Directory.CreateDirectory(artifactsDir).FullName} --nologo");
+    // Uninstall BullseyeBuild.Template
+    await RunAsync("dotnet", "new --uninstall BullseyeBuild.Template");
+
+    // Install BullseyeBuild.Template
+    await RunAsync("dotnet", $"new --install {Path.Combine(artifactsDir, "BullseyeBuild.Template*.nupkg")}");
 });
 
-Target("default", DependsOn(Targets.GenerateArtifacts));
+Target("default", DependsOn(Targets.RunTests));
 
 await RunTargetsAndExitAsync(args);
 
@@ -21,4 +27,5 @@ internal static class Targets
 {
     public const string CleanArtifactsOutput = "clean-artifacts-output";
     public const string GenerateArtifacts = "generate-artifacts";
+    public const string RunTests = "run-tests";
 }
