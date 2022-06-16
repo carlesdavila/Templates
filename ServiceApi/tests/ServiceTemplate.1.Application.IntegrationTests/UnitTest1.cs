e33a977996423ecd4fc553c42303b745897ed05e
Carlos Davila <carlesdavila@users.noreply.github.com>
2022-06-16 21:52:01 +0200

    added test projects

---
+++ ServiceApi/tests/ServiceTemplate.1.Application.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace ServiceTemplate._1.Application.IntegrationTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ ServiceApi/tests/ServiceTemplate.1.Integration.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace ServiceTemplate._1.Integration.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
